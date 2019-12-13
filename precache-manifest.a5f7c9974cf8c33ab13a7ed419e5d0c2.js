04db9c7437dbadcf521f4f7d646a02ced9ba6abb
Patricio Lopez J <lopezjuripatricio@gmail.com>
2019-12-13 16:57:55 -0300

    Updates

---
+++ precache-manifest.a5f7c9974cf8c33ab13a7ed419e5d0c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab4e29f5c696c8a1c467c59d99ed496",
+    "url": "/react-ethereum/index.html"
+  },
+  {
+    "revision": "06a398651384a31d51e3",
+    "url": "/react-ethereum/static/js/2.44d65d65.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-ethereum/static/js/2.44d65d65.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c00507a88355f78422e9",
+    "url": "/react-ethereum/static/js/main.27c852c1.chunk.js"
+  },
+  {
+    "revision": "da157eadf5a2aada7205",
+    "url": "/react-ethereum/static/js/runtime-main.ea844c01.js"
+  }
+]);
